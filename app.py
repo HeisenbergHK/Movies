1e5c387e557d15037abf3f849e16b1c6abdae4b5
Hassan Kalantari <hasan.kalantari29@gmail.com>
2025-11-02 05:42:47 +0330

    core(API): Creating API endpoint in flask + building essential functionalities in utils module.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+
+from utils import search_top_5
 
 app = Flask(__name__)
 
@@ -6,3 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello movies!"
+
+
+@app.route("/api/v1/top_match")
+def return_top_matches():
+    keyword = request.args.get("keyword")
+    if not keyword:
+        return {"error": "Missing 'keyword' parameter"}, 400  # Bad Request
+
+    result = search_top_5(keyword)
+    return result
---
+++ utils.py
@@ -0,0 +1,39 @@
+import math
+
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+
+def sort_key(item):
+    # If imdbs is not a number (nan), use hit instead
+    if math.isnan(item["imdb"]) or item["imdb"] == 0:
+        return (
+            item["hit"] / 10
+        )  # Instead of 0-100, it should be between 0-10, so we divide by 10
+    return item["imdb"]
+
+
+def search_top_5(prefix_keyword):
+    result = []
+    seen = set()
+
+    while True:
+        if len(result) >= 5 or not prefix_keyword:
+            break
+
+        filtered_df = df[df["series_name"].str.startswith(prefix_keyword)]
+        temp = filtered_df.to_dict(orient="records")
+
+        sorted_list = sorted(temp, key=sort_key, reverse=True)
+
+        # Only add unique items
+        for item in sorted_list:
+            title = item["series_name"]
+            if title not in seen:
+                seen.add(title)
+                result.append(item)
+
+        prefix_keyword = prefix_keyword[:-1]
+
+    return result[:5]
